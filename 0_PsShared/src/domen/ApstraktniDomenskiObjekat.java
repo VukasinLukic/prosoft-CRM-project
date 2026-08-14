510f687643d34aec46a0a075bdc598552cc8216f
VukasinLukic <vukasin4sports@gmail.com>
2026-08-14 13:14:56 +0200

    inicijalni push, napravljeni projekti

---
+++ 0_PsShared/src/domen/ApstraktniDomenskiObjekat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domen;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Vukasin Lukic
+ */
+public interface ApstraktniDomenskiObjekat extends Serializable{
+    //Interfejs koje domenske klase implementiraju!
+    
+    
+}
